f0f1a7945133ec3e45b919cd8b6cdefe47a5325c
WildWuzzy <jtj.lord@gmail.com>
2020-04-27 21:32:12 +0200

    lensflare try

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import './style.css';
 import { createSkybox } from './skybox';
 
+
+
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 55, window.innerWidth/window.innerHeight, 45, 30000 );
 camera.position.set(-900, -200, -900);
@@ -17,7 +19,41 @@ let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
 let skybox = createSkybox();
-scene.add(skybox);
+//scene.add(skybox);
+
+
+var light = new THREE.PointLight( 0xff0000, 100, 0 );
+light.position.set( -30,80,250 );
+//scene.add( light );
+var sphereSize = 1;
+var pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
+
+
+
+// var textureLoader = new THREE.TextureLoader();
+
+// var textureFlare0 = textureLoader.load( "textures/lensflare/lensflare0.png" );
+// var textureFlare1 = textureLoader.load( "textures/lensflare/lensflare2.png" );
+// var textureFlare2 = textureLoader.load( "textures/lensflare/lensflare3.png" );
+
+// var lensflare = new Lensflare();
+
+// lensflare.addElement( new LensflareElement( textureFlare0, 512, 0 ) );
+// lensflare.addElement( new LensflareElement( textureFlare1, 512, 0 ) );
+// lensflare.addElement( new LensflareElement( textureFlare2, 60, 0.6 ) );
+
+// light.add( lensflare );
+
+
+//scene.add(pointLightHelper);
+
+let skyboxGroup = new THREE.Group();
+skyboxGroup.add(skybox);
+skyboxGroup.add(light);
+skyboxGroup.add(pointLightHelper);
+
+scene.add(skyboxGroup);
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -27,7 +63,7 @@ function animate() {
     camera.lookAt( scene.position );
 
 
-    skybox.rotation.x += 0.0005;
+    skyboxGroup.rotation.x += 0.0005;
     skybox.rotation.y += 0.0005;
 
     renderer.render(scene, camera);
