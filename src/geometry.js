766498983f265e8d5447748088e582a1ea0f7acd
WildWuzzy <jtj.lord@gmail.com>
2020-10-19 11:12:30 +0200

    Remove unused arrow function

--- src/geometry.js
+++ src/geometry.js
@@ -1,23 +1,6 @@
 import * as THREE from 'three';
 import Jimmy from './images/Jimmy_0238_1400px.jpg';
 
-export function createArrow() {
-    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-    const points = [
-        new THREE.Vector3( -10, 0, 0 ),
-        new THREE.Vector3( 0, 10, 0 ),
-        new THREE.Vector3( 10, 0, 0 ),
-        new THREE.Vector3( -10, 0, 0 )
-    ];
-
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-    const line = new THREE.Line( geometry, material );
-
-    return line;
-}
-
 export function createJimmyCube(name) {
     const texture = new THREE.TextureLoader().load(Jimmy);
     const material = new THREE.MeshBasicMaterial( { map: texture } );	
